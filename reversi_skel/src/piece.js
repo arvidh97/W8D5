9f2c5b859fcb191e09f97447db325cae8301e1b0
Arvid Hossain <arvidh97@gmail.com>
2023-04-28 13:37:28 -0400

    added piece functions

--- reversi_skel/src/piece.js
+++ reversi_skel/src/piece.js
@@ -2,18 +2,27 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if (this.color === 'black') {
+        return 'white';
+    }
+    else {
+        return 'black';
+    };
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    // let that = this
+    this.color = this.oppColor() //opp color this can also be that
 };
 
 /**
@@ -21,6 +30,12 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+    if (this.color === 'black') {
+        return 'B';
+    }
+    else {
+        return 'W';
+    };
 };
 
 // DON'T TOUCH THIS CODE
